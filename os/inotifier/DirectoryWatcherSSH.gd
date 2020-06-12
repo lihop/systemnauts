be7db7914da9ab492de5e0a179f6ee01165d3c1c
Leroy Hopson <git@leroy.geek.nz>
2020-06-12 14:46:18 +0700

    Remove a debug print

--- os/inotifier/DirectoryWatcherSSH.gd
+++ os/inotifier/DirectoryWatcherSSH.gd
@@ -8,7 +8,6 @@ var ssh_connection: NodePath
 func _ready():
 	var _ssh_stream := preload("res://addons/os_utils/SSHStream.tscn").instance()
 	_ssh_stream.command = Inotifier.COMMAND_FORMAT % directory
-	print("COMMAND!!!!!: ", _ssh_stream.command)
 	_ssh_stream.ssh_connection = ssh_connection
 	_ssh_stream.connect("data_received", self, "_parse")
 	add_child(_ssh_stream)
