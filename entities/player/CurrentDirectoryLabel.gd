82dfaf9dba0f7285e16b873bb6745ef0f9505288
Leroy Hopson <git@leroy.geek.nz>
2020-06-18 13:55:35 +0700

    Remove debug code

--- entities/player/CurrentDirectoryLabel.gd
+++ entities/player/CurrentDirectoryLabel.gd
@@ -11,17 +11,13 @@ func _ready():
 	_shell = get_node_or_null(shell)
 	
 	if _shell:
-		print("Connectiing shell")
 		_shell.connect("pid_updated", self, "_set_label")
 		_shell.connect("directory_changed", self, "_on_directory_changed")
-		print(_shell)
-		print(_shell.get_signal_connection_list("directory_changed"))
 	else:
 		push_warning("shell %s is null. Label won't work" % shell)
 
 
 func _on_directory_changed(new_directory):
-	print("it changed!: ", new_directory)
 	text = new_directory
 
 
