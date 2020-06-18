1d757fb627d904b023a7ec58e6923fe9c906f8f7
Leroy Hopson <git@leroy.geek.nz>
2020-06-18 13:52:07 +0700

    Improve how pid is set for shell process
    Instead of creating a file on the server we now echo the shells pid
    environment delimited by a magic string so we can easily parse it in the
    shells output. This technique is faster and more reliable.
    
    Closes #8

--- systems/os/Shell.gd
+++ systems/os/Shell.gd
@@ -10,6 +10,9 @@ const SpecialCharacters = {
 
 const CHAR_INTR = SpecialCharacters.INTR
 
+const MAGIC_PID_DELIMITER = "__MAGIC_PID_DELIMITER__"
+
+signal typed(text)
 signal data_received(data)
 signal directory_changed(new_directory)
 
@@ -22,24 +25,43 @@ var _pid_file
 func _ready():
 	_shell_stream = get_node(shell_stream)
 	
-	# Get the process id of this shell.
-	_set_pid()
+	var pid_regex = RegEx.new()
+	pid_regex.compile(".*%s(?<pid>[0-9]+)%s.*" % [MAGIC_PID_DELIMITER, MAGIC_PID_DELIMITER])
 	
-	# Wait until we have the pid, before connecting the terminal output.
-	yield(self, "pid_updated")
-	reset()
+	yield(_shell_stream, "data_received")
+	yield(_shell_stream, "data_received")
+	_shell_stream.put_string("echo %s$$%s" % [MAGIC_PID_DELIMITER, MAGIC_PID_DELIMITER])
+	yield(_shell_stream, "data_received")
+	_shell_stream.put_string("\n")
+	var data = yield(_shell_stream, "data_received")
+	var matches = pid_regex.search(data.get_string_from_utf8())
+	
+	if matches:
+		pid = int(matches.get_string("pid"))
+	else:
+		push_error("Could not get pid for shell %s" % get_instance_id())
+	
+	yield(reset(), "completed")
+	
+	# Finally, we can expose the internal _shell_stream now that everything has
+	# been set up.
 	_shell_stream.connect("data_received", self, "_on_data_received")
 
 
 func type(text: String):
 	_shell_stream.put_string(text)
+	emit_signal("typed", text)
 
 
 func reset():
 	# Stop whatever process is currently running in the foreground (if any).
-	type(CHAR_INTR)
+	_shell_stream.put_string(CHAR_INTR)
+	yield(_shell_stream, "data_received")
+	
 	# Reset the terminal.
-	type("reset\n")
+	_shell_stream.put_string("reset")
+	yield(_shell_stream, "data_received")
+	_shell_stream.put_string("\n")
 
 
 func change_directory(absolute_path: String) -> void:
@@ -50,29 +72,3 @@ func change_directory(absolute_path: String) -> void:
 
 func _on_data_received(data: PoolByteArray):
 	emit_signal("data_received", data)
-
-
-# Get the process id of this shell. Currently works in a very roundabout fashion.
-func _set_pid():
-	_pid_file = "godot-shell-%s-pid" % get_instance_id()
-	
-	$"/root/VM/Inotifier".add_file_event_handler("/tmp", _pid_file, self,
-			"_pid_file_created", Inotifier.EVENT_CREATE)
-	
-	yield(get_tree().create_timer(1),"timeout")
-	_shell_stream.put_string("echo $$ > /tmp/%s\n" % _pid_file)
-
-
-func _pid_file_created():
-	var output = []
-	var exit_code = yield(VM.execute("cat", ["/tmp/%s" % _pid_file], output), "completed")
-	
-	if exit_code == 0:
-		print("got pid: ", output)
-		pid = int(output[0])
-		emit_signal("pid_updated", pid)
-	else:
-		print("Did not get pid")
-		print("error: %d" % exit_code)
-		print(output)
-		pid = 1
