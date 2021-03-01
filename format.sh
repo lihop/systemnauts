480682b7f4b656520a8ab706b73222547dca6298
Leroy Hopson <git@leroy.geek.nz>
2021-03-01 14:14:27 +0700

    Update format script to work with git hooks
    If the format script has not previously be run manually it will fail
    when git tries to run it as a hook. This fixes that.

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
-if ! source .venv/bin/activate; then python -m venv .venv && source .venv/bin/activate; fi
+if [ ! -f .venv/bin/activate ] || ! source .venv/bin/activate; then python -m venv .venv && source .venv/bin/activate; fi
 if ! gdformat --version; then pip install -r requirements.txt; fi
 if [ -z $1 ]; then find game/ -type f -name "*.gd" | xargs gdformat; else gdformat $1; fi
