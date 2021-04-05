f06d5d32fc0fa68ca226166063e44fe391ddd879
Leroy Hopson <git@leroy.geek.nz>
2021-04-05 16:40:32 +0700

    Automatic updates

--- game/systems/network_map/ZMap.cs
+++ game/systems/network_map/ZMap.cs
@@ -4,14 +4,14 @@ using Godot;
 public class ZMap : Node
 {
 	[Signal]
-    public delegate void HostFound(string ip_address);
-    
+	public delegate void HostFound(string ip_address);
+	
 	public async void Scan()
-    {
-        void StdOutHandler(string ip_address) => EmitSignal(nameof(HostFound), ip_address);
+	{
+		void StdOutHandler(string ip_address) => EmitSignal(nameof(HostFound), ip_address);
 
-        var cmd = Cli.Wrap("zmap").WithArguments(new[] {"-p", "80", "-q", "-v0", "-n", "256"}) | (StdOutHandler);
+		var cmd = Cli.Wrap("zmap").WithArguments(new[] {"-p", "80", "-q", "-v0", "-n", "256"}) | (StdOutHandler);
 
-        var result = await cmd.ExecuteAsync();
-    }
+		var result = await cmd.ExecuteAsync();
+	}
 }
