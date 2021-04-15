ffda7079aba79fc8a4d551b1b47892f6606f1d27
Leroy Hopson <git@leroy.geek.nz>
2021-04-15 11:46:59 +0700

    Remove some old/unused files

--- game/tests/integration/other/other.test.gd
+++
@@ -1,9 +0,0 @@
-extends "res://addons/gut/test.gd"
-
-
-func test_thing():
-	var thing = preload("other.tscn")
-	var blah = thing.instance()
-	watch_signals(blah)
-	blah.Emit()
-	assert_signal_emitted_with_parameters(blah, "Hah", [])
